a43d00d999d2eec3c6ec26c7ffc84c103210a63e
TienDQ <quangtiendang@vnpt.vn>
2023-10-06 16:53:39 +0700

    update

--- src/components/PlayBox.vue
+++ src/components/PlayBox.vue
@@ -17,7 +17,7 @@
                 class="box-form"
                 style="display: flex; justify-content: space-between"
             >
-                <div
+                <!-- <div
                     class="btn btn-icon"
                     @click="changeGoalMatrix"
                     title="Thay đổi ma trận đích"
@@ -33,7 +33,7 @@
                     title="Xác nhận ma trận đích"
                 >
                     <icon icon="gg:check-o" height="25px" />
-                </div>
+                </div> -->
                 <div
                     class="btn btn-icon"
                     @click="sufferStartMatrix"
@@ -47,6 +47,20 @@
                     title="Tự sắp xếp ma trận ban đầu"
                 >
                     <icon icon="game-icons:jigsaw-piece" height="25px" />
+                </div>
+                <div
+                    class="btn btn-icon"
+                    @click="confirmDone"
+                    title="Xác nhận ma trận ban đầu và đích"
+                >
+                    <icon icon="gg:check-o" height="25px" />
+                </div>
+                <div
+                    class="btn btn-icon"
+                    @click="cancelPickingMatrix"
+                    title="Huỷ"
+                >
+                    <icon icon="game-icons:cancel" height="25px" />
                 </div>
                 <div
                     class="btn btn-icon"
@@ -83,17 +97,22 @@
                             <drop
                                 v-for="(cell, cellIndex) in row"
                                 :key="cellIndex"
-                                @drop="dropEvent($event,rowIndex, cellIndex)"
+                                @drop="dropEvent($event, rowIndex, cellIndex)"
                                 mode="cut"
                             >
-                            <drag v-if="cell" :data="cell" @cut="remove(rowIndex,cellIndex)">
-                                <Piece
-                                    :value="cell"
-                                    :type="1"
-                                    :size="50"
-                                    :active="true"
+                                <drag
+                                    v-if="cell"
+                                    :data="cell"
+                                    @cut="remove(rowIndex, cellIndex)"
+                                >
+                                    <Piece
+                                        :value="cell"
+                                        :type="1"
+                                        :size="50"
+                                        :active="true"
                                 /></drag>
-                                <Piece v-else
+                                <Piece
+                                    v-else
                                     :value="cell"
                                     :type="1"
                                     :size="50"
@@ -109,15 +128,6 @@
                 v-show="selfPickingStart"
                 style="display: flex; align-items: center"
             >
-                <div
-                    style="height: 25px; width: 25px"
-                    class="btn r-btn btn-icon"
-                    @click="confirmStartMatrix"
-                    title="Sử dụng AI"
-                    :disabled="pieceToPick.length != 1"
-                >
-                    <icon icon="material-symbols:done-rounded" height="15px" />
-                </div>
                 <div class="piece-to-pick">
                     <drag
                         v-for="(piece, index) in pieceToPick"
@@ -152,12 +162,12 @@ export default {
             goalMatrix: [
                 [1, 2, 3],
                 [4, 5, 6],
-                [0, 7, 8],
+                [7, 8, 9],
             ],
             startMatrix: [
                 [1, 2, 3],
                 [4, 5, 6],
-                [0, 7, 8],
+                [7, 8, 9],
             ],
             pieceToPick: [],
             editGoal: false,
@@ -169,26 +179,51 @@ export default {
             currentPiece: null,
             top: null,
             left: null,
+            confirmDonePrepare: false,
         }
     },
     methods: {
-        dropEvent(e,rowIndex, cellIndex) {
-          if(this.startMatrix[rowIndex][cellIndex] != 0) this.currentPiece = this.startMatrix[rowIndex][cellIndex]
+        dropEvent(e, rowIndex, cellIndex) {
+            if (this.startMatrix[rowIndex][cellIndex] != 0)
+                this.currentPiece = this.startMatrix[rowIndex][cellIndex]
             this.startMatrix[rowIndex][cellIndex] = e.data
-          },
-          returnToQueue(n){
-            if(this.currentPiece) this.pieceToPick.push(this.currentPiece)
+        },
+        returnToQueue(n) {
+            if (this.currentPiece) this.pieceToPick.push(this.currentPiece)
             const index = this.pieceToPick.indexOf(n)
             if (index > -1) {
                 this.pieceToPick.splice(index, 1)
             }
-            this.currentPiece = null;
-          },
-          remove(rowIndex, cellIndex) {
-            if(this.currentPiece) this.startMatrix[rowIndex][cellIndex] = this.currentPiece
+            this.currentPiece = null
+        },
+        remove(rowIndex, cellIndex) {
+            if (this.currentPiece)
+                this.startMatrix[rowIndex][cellIndex] = this.currentPiece
             else this.startMatrix[rowIndex][cellIndex] = 0
-            this.currentPiece = null;
+            this.currentPiece = null
             // console.log(this.startMatrix);
+        },
+        confirmDone() {
+            if (this.selfPickingStart) {
+                if (this.pieceToPick.length != 1) {
+                    this.$toast.error('Chưa chọn xong ma trận ban đầu !')
+                    return
+                } else {
+                    const leftPiece = this.pieceToPick[0]
+                    //disable this in goal matrix
+                    for (let i = 0; i < this.goalMatrix.length; i++) {
+                        for (let j = 0; j < this.goalMatrix[i].length; j++) {
+                            if (this.goalMatrix[i][j] == leftPiece) {
+                                this.goalMatrix[i][j] = 0
+                                break
+                            }
+                        }
+                    }
+                    this.pieceToPick = []
+                    this.confirmDonePrepare = true
+                    this.selfPickingStart = false
+                }
+            }
         },
         changeGoalMatrix() {
             this.editGoal = true
@@ -199,9 +234,42 @@ export default {
             this.goalMatrix = this.$refs.goalPuzzle.matrix
             this.goalMatrixActive = false
         },
+        cancelPickingMatrix() {
+            this.selfPickingStart = false
+            this.pieceToPick = []
+            this.startMatrix = [
+                [1, 2, 3],
+                [4, 5, 6],
+                [0, 7, 8],
+            ]
+        },
+        isCompeleteMatrix(m){
+            //find if matrix not have 0 retrun false
+            const fm = m.flat()
+            for(let i = 0; i < fm.length; i++){
+                if(fm[i] == 0){
+                    return true
+                }
+            }
+            return false
+        },
+        removeAPiece(n) {
+            
+        },
         sufferStartMatrix() {
+            if (this.selfPickingStart) {
+                console.log(this.$toast)
+                this.$toast.error('Đang sắp xếp ma trận bắt đầu bằng kéo thả !')
+
+                return
+            }
+            else if(!this.isCompeleteMatrix(this.startMatrix)){
+                this.$toast.error('Chọn phần lược bỏ trên ma trận bắt đầu !')
+                this.startMatrixActive = true
+                return
+            }
+            const remain = this.startMatrix.flat()
             this.startMatrix = []
-            const remain = [0, 1, 2, 3, 4, 5, 6, 7, 8]
             for (let i = 0; i < 3; i++) {
                 this.startMatrix.push([])
                 for (let j = 0; j < 3; j++) {
@@ -221,7 +289,7 @@ export default {
             this.selfPickingStart = true
         },
         confirmStartMatrix() {
-          if(this.pieceToPick.length != 1) return
+            if (this.pieceToPick.length != 1) return
             this.selfPickingStart = false
             this.startMatrixType = 1
         },
--- src/main.js
+++ src/main.js
@@ -2,8 +2,14 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import Icon from './components/Icon.vue'
+import ToastPlugin from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-bootstrap.css';
 
 const app = createApp(App)
 app.component('icon', Icon);
+app.use(ToastPlugin,{
+  position: 'top-right',
+  duration: 2000,
+})
 app.mount('#app')
 // createApp(App).mount('#app')
